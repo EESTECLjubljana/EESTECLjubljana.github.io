b5418a585fec35aaa251d7a387b36bd945b54869
EESTECLjubljana <36571385+EESTECLjubljana@users.noreply.github.com>
2018-02-17 18:36:12 +0100

    css

---
+++ delavnice/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.eventbrite.com/e/jobfair-treningi-delavnice-tickets-16977859234");
+die();
+?>
---
+++ feedbackPodjetja/index.php
@@ -0,0 +1,4 @@
+<?php
+ header("Location: https://docs.google.com/forms/d/19wkrKRvjVMGyBZb6yaPQlwbwrpv6K9eJAW6cj_VfYe4/viewform");
+ die();
+?>
